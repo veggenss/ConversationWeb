b6c4c71365fbb28e70ad4c0d4154ea1cc28ac4e5
isakbh <isak@brunhenriksen.net>
2025-05-23 17:31:47 +0200

    Fikset link til Samtaler På Nett og en liten notat/reminder om å lage logo til SPN (Samtaler På Nett)

--- samtalerpanett/main.php
+++ samtalerpanett/main.php
@@ -48,7 +48,7 @@ if(!isset($_SESSION['user_id'])){
     <title>Samtaler på nett <?php echo $version; ?></title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
     <link rel="stylesheet" href="CSS/mainStyle.css" />
-    <!-- logoet til toner på nett som favicon -->
+    <!-- logoet til toner på nett som favicon !! LAG SAMTALER PÅ NETT LOGO OG LEGG DET TIL HER ISTEDET FOR TONER PÅ NETT LOGO - Isak 23.05.25!!-->
     <link rel="icon" href="assets/icons/logo.png" />
     <!-- ikoner fra font awesome og google fonts-->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" />
@@ -57,13 +57,13 @@ if(!isset($_SESSION['user_id'])){
     <div class="activity-viewer">
         <div class="activity-list">
             <h3>Aktive venner</h3>
-        
+
         </div>
     </div>
     <div class="container">
         <h1 id="header">Samtaler på Nett</h1>
 
-        <p>Her kommer den snart berømte smsappen, Samtaler på Nett.</p>
+        <p>Her kommer det snart berømte chatteprogrammet, Samtaler på Nett.</p>
     </div>
 </body>
 </html>
