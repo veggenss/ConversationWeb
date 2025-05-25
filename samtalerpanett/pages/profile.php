dc1391d746be24eaad6cc8e3d5c50598d23c17fb
veggenss <veggens.v@gmail.com>
2025-05-25 02:15:52 +0200

    Man kan endre brukernavn og email i profil Når email blir endret sendes en ny verification mail til den adressen (LETS GOO)

--- samtalerpanett/pages/profile.php
+++ samtalerpanett/pages/profile.php
@@ -1,6 +1,87 @@
 <?php
 session_start();
+if (!isset($_SESSION['user_id'])) {
+    // hvis brukeren ikke er logget inn, redirect til login
+    header("Location: login.php");
+    exit();
+}
 include '../include/db.inc.php';
+if($_SERVER['REQUEST_METHOD'] == 'POST'){
+    $sql = "SELECT * FROM users WHERE id = ?";
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param("i", $_SESSION['user_id']);
+    $stmt->execute();
+    $result = $stmt->get_result();
+    $user = $result->fetch_assoc();
+
+
+    $new_username = $_POST['username'];
+    $new_email = $_POST['email'];
+
+    $email = trim($new_email);
+    if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
+        $error = "Ugyldig e-post";
+    }
+    // sjekker at domenet til eposten finnes
+    elseif(!checkdnsrr(substr(strrchr($email, "@"), 1), "MX")){
+        $error = "E-postdomenet finnes ikke";
+
+    }
+    elseif(!preg_match('/^.{4,}$/', $new_username)){
+        $error = "Brukernavnet må være 4 siffer eller mer";
+    }
+    elseif($new_email !== $user['mail']){
+        // Sjekk om epost allerede finnes
+        $sql = "SELECT id FROM users WHERE mail = ? AND id != ?";
+        $stmt = $conn->prepare($sql);
+        $stmt->bind_param("si", $new_email, $_SESSION['user_id']);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        if ($result->num_rows > 0) {
+            $error = "E-posten er allerede i bruk";
+        }
+    }
+
+
+    if(!isset($error)){
+        // hvis Email er anderledes en den i db oppdaterer den email også sender ny email verification
+        if($new_email !== $user['mail']){ 
+
+            $token = bin2hex(random_bytes(16));
+
+            $sql = "UPDATE users SET username = ?, mail = ?, email_verification_token = ?, email_verified = 0 WHERE id = ?";
+            $stmt = $conn->prepare($sql);
+            $stmt->bind_param("sssi", $new_username, $new_email, $token, $_SESSION['user_id']);
+            if($stmt->execute()){
+                require '../send_email_verification.php';
+                if(sendVerificationEmail($new_email, $new_username, $token)){
+                    session_unset();
+                    session_destroy();
+                    header("Location: ../login.php");
+                    exit();
+                }
+                else{
+                    $error = "Kunne ikke sende e-post";
+                }
+            }
+        }
+        else{
+            // oppdaterer bare brukernavn siden e-post er det samme
+            $sql = "UPDATE users SET username = ? WHERE id = ?";
+            $stmt = $conn->prepare($sql);
+            $stmt->bind_param("si", $new_username, $_SESSION['user_id']);
+
+            if($stmt->execute()){
+                $message = "Oppdaterte bruker";
+                $_SESSION['username'] = $new_username;
+                $_SESSION['email'] = $new_email;
+            }
+            else{
+                $error = "Kunne ikke oppdatere bruker";
+            }
+        } 
+    }
+}
 
 ?>
 
@@ -18,9 +99,12 @@ include '../include/db.inc.php';
 
 <body>
     <div class="auth-con">
+        <h2><?php echo htmlspecialchars($_SESSION["username"]); ?>'s profil</h2>
+        <?php if (isset($error)):?>
+        <div class="error"><?php echo "{$error}<br>"; ?></div>
+        <?php endif; ?>
         <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']);?>" method="POST">
-            <h2><?php echo htmlspecialchars($_SESSION["username"]); ?>'s profil</h2>
-
+        
             <!-- @IsakBH Hadde vært fint om du vile lagt til knapp og visning av profil bilde. Også må du vel lage nye rows i DB så bare paste her eller send meg SQL koden til det ;) --Viggo 24/05 22:00 -->
             <div class="profile-group">
                 <img src="uploads/<?php echo htmlspecialchars($_SESSION["profile_picture"]); ?>" alt="Profilbilde">
@@ -28,20 +112,22 @@ include '../include/db.inc.php';
 
             <div class="profile-group">
                 <label>Brukernavn:</label>
-                <input type="text" placeholder="brukernavn" name="username" value="<?php echo $_SESSION['username']?>">
+                <input type="text" placeholder="brukernavn" name="username" value="<?php echo htmlspecialchars($_SESSION['username']);?>">
             </div>
 
             <div class="profile-group">
                 <label>E-post:</label>
-                <input type="text" value="<?php echo $_SESSION['email'];?>">
+                <input type="text" name="email" value="<?php echo htmlspecialchars($_SESSION['email']);?>">
             </div>
             
             <div class="profile-group">
                 <p>Glemt Passord? <br><a id="backButton" href="password_reset.php">Tilbakestill Passord <i class="fa-solid fa-arrow-up-right-from-square"></i></a></p>
             </div>
-            <button id="submit" type="submit">Lagre Endringer</button>
+            <button id="submit" type="submit" onclick="return confirm('Hvis du har endret email: \nDu blir logget ut også må du verifisere mail på nytt før du logger in igjen')">Lagre Endringer</button>
         </form>
-        <br>
+        <?php if(isset($message)):?>
+        <div class="positive"><?php echo $message?></div>
+        <?php endif;?>
         <p>Antall samtalepoeng: <?php // samtalepoeng går her ?></p>
         <a id="backButton" href="/projects/samtalerpanett/main.php">Tilbake til Samtaler På Nett</a>
     </div>
