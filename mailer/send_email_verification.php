8de37db46388d1970ce0cbcf592b6423945473ff
IsakBH <isak@brunhenriksen.net>
2025-10-27 21:12:22 +0100

    Endret "aa" med "å" - det trengs ikke lenger pga. UTF-8 charset. vet ikke hvorfor jeg ikke har endret det for flere måneder siden

--- mailer/send_email_verification.php
+++ mailer/send_email_verification.php
@@ -17,10 +17,10 @@ function sendVerificationEmail($to, $username, $token, $config){
         $mail->Port = $config['mail']['port'];
 
 
-        $mail->setFrom('samtalerpaanett@gmail.com', 'Chat Paa Nett');
+        $mail->setFrom('samtalerpaanett@gmail.com', 'Chat På Nett');
         $mail->addAddress($to);
         $mail->isHTML(true);
-        $mail->Subject = 'Bekreft e-posten din hos Chat Paa Nett';
+        $mail->Subject = 'Bekreft e-posten din hos Chat På Nett';
 
         $protocol = (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] != 'off') ? "https" : "http";
         $host = $_SERVER['HTTP_HOST'];
